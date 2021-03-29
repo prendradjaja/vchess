ad6d040b5a94dd79150af84d2c97cee940e0050b
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-03-29 13:23:46 -0700

    NOMERGE: Replace Myexample with a minimal example of adding a piece

--- client/src/variants/Myexample.js
+++ client/src/variants/Myexample.js
@@ -1,65 +1,94 @@
 import { ChessRules } from "@/base_rules";
 
-// Copied from Hoppelpoppel
 export class MyexampleRules extends ChessRules {
 
-  getSlideNJumpMoves_([x, y], steps, oneStep, options) {
-    options = options || {};
-    let moves = [];
-    outerLoop: for (let step of steps) {
-      let i = x + step[0];
-      let j = y + step[1];
-      while (V.OnBoard(i, j) && this.board[i][j] == V.EMPTY) {
-        if (!options.onlyTake) moves.push(this.getBasicMove([x, y], [i, j]));
-        if (!!oneStep) continue outerLoop;
-        i += step[0];
-        j += step[1];
+  static get PawnSpecs() {
+    return Object.assign(
+      {},
+      ChessRules.PawnSpecs,
+      {
+        promotions:
+          ChessRules.PawnSpecs.promotions
+          .concat([V.EMPRESS, /* ADD_MORE_PIECES_HERE */])
       }
-      if (V.OnBoard(i, j) && this.canTake([x, y], [i, j]) && !options.onlyMove)
-        moves.push(this.getBasicMove([x, y], [i, j]));
+    );
+  }
+
+  getPpath(b) {
+    if (b[1] === 'e') {
+      return "Perfect/" + b;
+    } else {
+      return b;
     }
-    return moves;
+    // return (
+    //   [V.AMAZON, V.EMPRESS, V.PRINCESS].includes(b[1])
+    //     ? "Perfect/"
+    //     : ""
+    // ) + b;
   }
 
-  getPotentialKnightMoves(sq) {
-    // The knight captures like a bishop
+  static GenRandInitFen(randomness) {
+    // TODO support randomness
+    return "rnbqkbne/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNE w 0 ahah -";
+  }
+
+
+  // Rook + knight
+  static get EMPRESS() {
+    return "e";
+  }
+
+  static get PIECES() {
+    return ChessRules.PIECES.concat([V.EMPRESS, /* ADD_MORE_PIECES_HERE */]);
+  }
+
+  getPotentialMovesFrom([x, y]) {
+    switch (this.getPiece(x, y)) {
+      case V.EMPRESS:
+        return this.getPotentialEmpressMoves([x, y]);
+      default:
+        return super.getPotentialMovesFrom([x, y]);
+    }
+  }
+
+  getPotentialEmpressMoves(sq) {
+    return this.getSlideNJumpMoves(sq, V.steps[V.ROOK]).concat(
+      this.getSlideNJumpMoves(sq, V.steps[V.KNIGHT], "oneStep")
+    );
+  }
+
+  isAttacked(sq, color) {
     return (
-      this.getSlideNJumpMoves_(
-        sq, ChessRules.steps[V.KNIGHT], "oneStep", { onlyMove: true })
-      .concat(
-        this.getSlideNJumpMoves_(
-          sq, ChessRules.steps[V.BISHOP], null, { onlyTake: true }))
+      super.isAttacked(sq, color) ||
+      this.isAttackedByEmpress(sq, color)
+      // ADD_MORE_PIECES_HERE
     );
   }
 
-  getPotentialBishopMoves(sq) {
-    // The bishop captures like a knight
+  isAttackedByEmpress(sq, color) {
     return (
-      this.getSlideNJumpMoves_(
-        sq, ChessRules.steps[V.BISHOP], null, { onlyMove: true })
-      .concat(
-        this.getSlideNJumpMoves_(
-          sq, ChessRules.steps[V.KNIGHT], "oneStep", { onlyTake: true }))
+      this.isAttackedBySlideNJump(sq, color, V.EMPRESS, V.steps[V.ROOK]) ||
+      this.isAttackedBySlideNJump(
+        sq,
+        color,
+        V.EMPRESS,
+        V.steps[V.KNIGHT],
+        "oneStep"
+      )
     );
   }
 
-  isAttackedByKnight([x, y], color) {
-    return super.isAttackedBySlideNJump(
-      [x, y],
-      color,
-      V.KNIGHT,
-      V.steps[V.BISHOP]
+  static get VALUES() {
+    return Object.assign(
+      // these values are just guesses
+      { e: 7 }, // ADD_MORE_PIECES_HERE
+      ChessRules.VALUES
     );
   }
 
-  isAttackedByBishop([x, y], color) {
-    return super.isAttackedBySlideNJump(
-      [x, y],
-      color,
-      V.BISHOP,
-      V.steps[V.KNIGHT],
-      "oneStep"
-    );
+  static get SEARCH_DEPTH() {
+    // Not sure if needed. Default is 3, but set to 2 in Perfect, Schess, and Colorbound.
+    return 2;
   }
 
 };
