bc8ef143a88878e2178c193d294903d331d87d44
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-04-02 16:52:47 -0700

    Remarkablerookies variant + other junk from the cwda branch
    This commit is the same as 30200b52332d87dda52ea33dcaa3ce7edadd483b on
    that branch

---
+++ client/src/variants/Cwdacc.js
@@ -0,0 +1,208 @@
+import { ChessRules, Move, PiPo } from "@/base_rules";
+import { ArrayFun } from "@/utils/array";
+import { randInt } from "@/utils/alea";
+
+export class CwdaccRules extends ChessRules {
+
+  static get PawnSpecs() {
+    return Object.assign(
+      {},
+      ChessRules.PawnSpecs,
+      {
+        promotions:
+          // ChessRules.PawnSpecs.promotions.concat(
+          [V.C_ROOK, V.C_KNIGHT, V.C_BISHOP, V.C_QUEEN]
+          // )
+      }
+    );
+  }
+
+  getPpath(b) {
+    if ([V.C_ROOK, V.C_KNIGHT, V.C_BISHOP, V.C_QUEEN].includes(b[1]))
+      return "Colorbound/" + b;
+    return b;
+  }
+
+  static GenRandInitFen(randomness) {
+    if (randomness == 0 || true /* TODO handle randomness */)
+      return "dhaskahd/pppppppp/8/8/8/8/PPPPPPPP/DHASKAHD w 0 ahah -";
+
+    // // Mapping white --> black (at least at start):
+    // const piecesMap = {
+    //   'r': 'd',
+    //   'n': 'h',
+    //   'b': 'a',
+    //   'q': 's',
+    //   'k': 'k'
+    // };
+    //
+    // const baseFen = ChessRules.GenRandInitFen(randomness);
+    // return (
+    //   baseFen.substr(0, 8).split('').map(p => piecesMap[p]).join('') +
+    //   baseFen.substr(8)
+    // );
+  }
+
+  static get C_ROOK() {
+    return 'd';
+  }
+  static get C_KNIGHT() {
+    return 'h';
+  }
+  static get C_BISHOP() {
+    return 'a';
+  }
+  static get C_QUEEN() {
+    return 's';
+  }
+
+  static get PIECES() {
+    return (
+      ChessRules.PIECES.concat([V.C_ROOK, V.C_KNIGHT, V.C_BISHOP, V.C_QUEEN])
+    );
+  }
+
+  getPotentialMovesFrom([x, y]) {
+    switch (this.getPiece(x, y)) {
+      case V.C_ROOK: return this.getPotentialC_rookMoves([x, y]);
+      case V.C_KNIGHT: return this.getPotentialC_knightMoves([x, y]);
+      case V.C_BISHOP: return this.getPotentialC_bishopMoves([x, y]);
+      case V.C_QUEEN: return this.getPotentialC_queenMoves([x, y]);
+      default: return super.getPotentialMovesFrom([x, y]);
+    }
+    return [];
+  }
+
+  static get steps() {
+    return Object.assign(
+      {},
+      ChessRules.steps,
+      {
+        // Dabbabah
+        'd': [
+          [-2, 0],
+          [0, -2],
+          [2, 0],
+          [0, 2]
+        ],
+        // Alfil
+        'a': [
+          [2, 2],
+          [2, -2],
+          [-2, 2],
+          [-2, -2]
+        ],
+        // Ferz
+        'f': [
+          [1, 1],
+          [1, -1],
+          [-1, 1],
+          [-1, -1]
+        ]
+      }
+    );
+  }
+
+  getPotentialC_rookMoves(sq) {
+    return (
+      this.getSlideNJumpMoves(sq, V.steps[V.BISHOP]).concat(
+      this.getSlideNJumpMoves(sq, V.steps['d'], "oneStep"))
+    );
+  }
+
+  getPotentialC_knightMoves(sq) {
+    return (
+      this.getSlideNJumpMoves(sq, V.steps['a'], "oneStep").concat(
+      this.getSlideNJumpMoves(sq, V.steps[V.ROOK], "oneStep"))
+    );
+  }
+
+  getPotentialC_bishopMoves(sq) {
+    return (
+      this.getSlideNJumpMoves(sq, V.steps['d'], "oneStep").concat(
+      this.getSlideNJumpMoves(sq, V.steps['a'], "oneStep")).concat(
+      this.getSlideNJumpMoves(sq, V.steps[V.BISHOP], "oneStep"))
+    );
+  }
+
+  getPotentialC_queenMoves(sq) {
+    return (
+      this.getSlideNJumpMoves(sq, V.steps[V.BISHOP]).concat(
+        this.getSlideNJumpMoves(sq, V.steps[V.KNIGHT], "oneStep"))
+    );
+  }
+
+  getCastleMoves([x, y]) {
+    const color = this.getColor(x, y);
+    const finalSquares = [
+      // Black castle long in an unusual way:
+      (color == 'w' ? [2, 3] : [1, 2]),
+      [V.size.y - 2, V.size.y - 3]
+    ];
+    return super.getCastleMoves([x, y], finalSquares);
+  }
+
+  isAttacked(sq, color) {
+    return (
+      super.isAttacked(sq, color) ||
+      this.isAttackedByC_rook(sq, color) ||
+      this.isAttackedByC_knight(sq, color) ||
+      this.isAttackedByC_bishop(sq, color) ||
+      this.isAttackedByC_queen(sq, color)
+    );
+  }
+
+  isAttackedByC_rook(sq, color) {
+    return (
+      this.isAttackedBySlideNJump(sq, color, V.C_ROOK, V.steps[V.BISHOP]) ||
+      this.isAttackedBySlideNJump(
+        sq, color, V.C_ROOK, V.steps['d'], "oneStep")
+    );
+  }
+
+  isAttackedByC_knight(sq, color) {
+    return (
+      this.isAttackedBySlideNJump(
+        sq, color, V.C_KNIGHT, V.steps[V.ROOK], "oneStep") ||
+      this.isAttackedBySlideNJump(
+        sq, color, V.C_KNIGHT, V.steps['a'], "oneStep")
+    );
+  }
+
+  isAttackedByC_bishop(sq, color) {
+    return (
+      this.isAttackedBySlideNJump(
+        sq, color, V.C_BISHOP, V.steps['d'], "oneStep") ||
+      this.isAttackedBySlideNJump(
+        sq, color, V.C_BISHOP, V.steps['a'], "oneStep") ||
+      this.isAttackedBySlideNJump(
+        sq, color, V.C_BISHOP, V.steps['f'], "oneStep")
+    );
+  }
+
+  isAttackedByC_queen(sq, color) {
+    return (
+      this.isAttackedBySlideNJump(sq, color, V.C_QUEEN, V.steps[V.BISHOP]) ||
+      this.isAttackedBySlideNJump(
+        sq, color, V.C_QUEEN, V.steps[V.KNIGHT], "oneStep")
+    );
+  }
+
+  static get VALUES() {
+    return Object.assign(
+      {},
+      ChessRules.VALUES,
+      {
+        d: 4,
+        h: 3,
+        a: 5,
+        s: 6
+      }
+    );
+  }
+
+  static get SEARCH_DEPTH() {
+    return 2;
+  }
+
+};
---
+++ client/src/variants/Myexample.js
@@ -0,0 +1,134 @@
+import { ChessRules } from "@/base_rules";
+
+export class MyexampleRules extends ChessRules {
+
+  static get PawnSpecs() {
+    return Object.assign(
+      {},
+      ChessRules.PawnSpecs,
+      {
+        promotions:
+          ChessRules.PawnSpecs.promotions
+          .concat([V.EMPRESS, /* ADD_MORE_PIECES_HERE */])
+      }
+    );
+  }
+
+  getPpath(b) {
+    if (b[1] === 'e') {
+      return "Perfect/" + b;
+    } else {
+      return b;
+    }
+    // return (
+    //   [V.AMAZON, V.EMPRESS, V.PRINCESS].includes(b[1])
+    //     ? "Perfect/"
+    //     : ""
+    // ) + b;
+  }
+
+  static GenRandInitFen(randomness) {
+    // TODO support randomness
+    // return "rnbqkbne/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNE w 0 ahah -";
+    return "k7/8/8/p7/P1P5/1P1P2E1/4P3/7K w 0 ahah -";
+  }
+
+
+  // Rook + knight
+  static get EMPRESS() {
+    return "e";
+  }
+
+  static get PIECES() {
+    return ChessRules.PIECES.concat([V.EMPRESS, /* ADD_MORE_PIECES_HERE */]);
+  }
+
+  getPotentialMovesFrom([x, y]) {
+    switch (this.getPiece(x, y)) {
+      case V.EMPRESS:
+        return this.getPotentialEmpressMoves([x, y]);
+      default:
+        return super.getPotentialMovesFrom([x, y]);
+    }
+  }
+
+  static get steps() {
+    return Object.assign(
+      {},
+      ChessRules.steps,
+      {
+        // I added "$" to avoid conflict with piece abbreviations (e.g. W for Woody Rook would conflict with W for Wazir)
+        // Dabbabah
+        '$d': [
+          [-2, 0],
+          [0, -2],
+          [2, 0],
+          [0, 2]
+        ],
+        // Wazir
+        '$w': [
+          [-1, 0],
+          [0, -1],
+          [1, 0],
+          [0, 1]
+        ],
+        // Alfil
+        '$a': [
+          [2, 2],
+          [2, -2],
+          [-2, 2],
+          [-2, -2]
+        ],
+        // Ferz
+        '$f': [
+          [1, 1],
+          [1, -1],
+          [-1, 1],
+          [-1, -1]
+        ],
+        // Threeleaper
+        '$3': [
+          [-3, 0],
+          [0, -3],
+          [3, 0],
+          [0, 3]
+        ],
+      }
+    );
+  }
+
+  getPotentialEmpressMoves(sq) {
+    return [].concat(
+      this.getSlideNJumpMovesLimited(sq, V.steps[V.ROOK], 4),
+    );
+  }
+
+  isAttacked(sq, color) {
+    return (
+      super.isAttacked(sq, color) ||
+      this.isAttackedByEmpress(sq, color)
+      // ADD_MORE_PIECES_HERE
+    );
+  }
+
+  isAttackedByEmpress(sq, color) {
+    return (
+      this.isAttackedBySlideNJumpLimited(sq, color, V.EMPRESS, V.steps[V.ROOK], 4) ||
+      false // || false can be removed later, doesn't do anything, is just here for ease of editing
+    );
+  }
+
+  static get VALUES() {
+    return Object.assign(
+      // these values are just guesses
+      { e: 7 }, // ADD_MORE_PIECES_HERE
+      ChessRules.VALUES
+    );
+  }
+
+  static get SEARCH_DEPTH() {
+    // Not sure if needed. Default is 3, but set to 2 in Perfect, Schess, and Colorbound.
+    return 2;
+  }
+
+};
